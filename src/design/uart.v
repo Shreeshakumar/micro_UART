b0ebf05e7649cd08a2662d31da43afbe910e50e0
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-11 20:12:04 +0530

    Refactor UART module and update submodule instantiations
    Refactor UART module input/output declarations and instantiate submodules.

--- src/design/uart.v
+++ src/design/uart.v
@@ -8,22 +8,40 @@ module baud (
 			sys_rst_l,			//main sys reset
 			xmitH,				//active high -> uart clock with baudrate pulse starts the trasmit process
 			uart_REC_dataH,		//asynchromous input data
-	input	[7:0]xmit_dataH,	//data to m=be sent, This data is sample when xmith high
+	input	[7:0]xmit_dataH,	//data to be sent, This data is sample when xmith high
 
 	output	uart_XMIT_dataH,	//ouput asynchronous transmitter
 			xmit_doneH,			//when active high thhis indictes that the xmit_dataH has been fully transmitted
-			rec_readyH,			//de-serialized recieved from the remote
-			rec_busy,			//indicates if the transmitter is actively transmitting a data
-			xmit_active,		//indicatedd if the receiver is currently busy
-	output	[7:0]rec_dataH,		//when high indicates fresh data is available on rec_dataH
+			rec_readyH,			//when high indicates fresh data is available on rec_dataH 
+			rec_busy,			//indicatedd if the receiver is currently busy
+			xmit_active,		//indicates if the transmitter is actively transmitting a data
+	output	[7:0]rec_dataH,		//de-serialized recieved from the remote
 	
 			);
 
+	/*(in inc.h)
+	DEFAULT PARAMETERS: 
+	parameter baudrate	 	= 2400;
+	parameter data_length 	= 8;
+	*/
 	
-	u_baud	baud();
+	wire 
 
-	u_xmit 	xmit();
+	u_baud	baud(
+		.sys_rst_l(sys_rst_l), .sys_clk(sys_clk),		//Main_inputs
+		.baud_ticks(baud_ticks)							//Wires
+		);
 
-	u_rec	rec();
+	u_xmit 	xmit(
+		.sys_rst(sys_rst_l), .xmitH(xmitH), .xmit_dataH(xmit_dataH),							//Main_inputs
+		.uart_XMIT_dataH(uart_XMIT_dataH), .xmit_doneH(xmit_doneH), .xmit_active(xmit_active)	//Main_outputs
+		.baud_ticks(baud_ticks)																	//Wires
+		);
+
+	u_rec	rec(
+		.sys_rst(sys_rst_l), .xmitH(xmitH), .uart_REC_dataH(uart_REC_dataH),		//Main_inputs
+		.rec_readyH(rec_readyH), .rec_busy(rec_busy), .rec_dataH(rec_dataH)			//Main_outputs
+		.baud_ticks(baud_ticks)														//Wires
+		);
 
 endmodule
