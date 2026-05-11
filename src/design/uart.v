2dbcca9bd1299f4a90ea4f9b60141f2bdf93e7f5
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-11 23:02:41 +0530

    Update uart.v

--- src/design/uart.v
+++ src/design/uart.v
@@ -8,14 +8,14 @@ module baud (
 			sys_rst_l,			//main sys reset
 			xmitH,				//active high -> uart clock with baudrate pulse starts the trasmit process
 			uart_REC_dataH,		//asynchromous input data
-	input	[7:0]xmit_dataH,	//data to be sent, This data is sample when xmith high
+	input	[data_len - 1:0]xmit_dataH,	//data to be sent, This data is sample when xmith high
 
 	output	uart_XMIT_dataH,	//ouput asynchronous transmitter
 			xmit_doneH,			//when active high thhis indictes that the xmit_dataH has been fully transmitted
 			rec_readyH,			//when high indicates fresh data is available on rec_dataH 
 			rec_busy,			//indicatedd if the receiver is currently busy
 			xmit_active,		//indicates if the transmitter is actively transmitting a data
-	output	[7:0]rec_dataH,		//de-serialized recieved from the remote
+	output	[data_len - 1:0]rec_dataH,		//de-serialized recieved from the remote
 	
 			);
 
