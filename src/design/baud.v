be556633de79508630b7dd0219e7882dc363f417
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-11 21:29:15 +0530

    Refactor baud module for improved functionality
    Refactor baud module to include separate counters for receiving and transmitting baud signals, and update logic for baud_rec and baud_xmit.

--- src/design/baud.v
+++ src/design/baud.v
@@ -3,34 +3,57 @@
 `include "inc.h"
 
 module baud (
-		input wire sys_rst_l,         //main sys reset
+		input wire 	sys_rst_l,         //main sys reset
         //wire xmitH,                   //active high -> uart clock with baudrate pulse starts the trasmit process
-        wire sys_clk,                 //main sys clock
+        			 sys_clk,                 //main sys clock
   
-		output reg baud_ticks							 //Wire
+		output reg baud_rec, baud_xmit						 //Wire
 		);
+	
+	/*(in inc.h)
+	DEFAULT PARAMETERS: 
+	parameter baudrate	 	= 2400;
+	parameter data_length 	= 8;
+	parameter clock_rate 	= 1000 000 000;  //100 Mhz	
+	parameter oversample 	= 16;
+	*/
 
-    reg [3:0]count;
-    
+	localparam cycles_rec = (1/(baudrate*oversample))*clock_rate ;//cycle is bit time * clock
+	localparam cycles_xmit = (1/baudrate)*clock_rate ;//cycle is bit time * clock
+
+	reg [$clog2(cycles_rec)-1:0] count_rec;
+	reg [$clog2(cycles_xmit)-1:0] count_xmit;
+	    
     always@(posedge sys_clk or posedge sys_rst_l)
         begin
         if (sys_rst_l)
             begin
-                baud_ticks <= 0;
-                count <= 0;
+                baud_rec = 0;
+                count_rec = 0;
             end
-            else if (count == 4'14)
+			else if (count_rec == cycles_rec)
                 begin
-                baud_ticks = ~baudticks;
-                count = count + 1;
+                baud_rec = 0;
+                count_rec = 0;
                 end
-            else if (count == 4'15)
+            else 
+                count_rec = count_rec + 1;
+        end
+
+	lways@(posedge sys_clk or posedge sys_rst_l)
+        begin
+        if (sys_rst_l)
+            begin
+                baud_xmit = 0;
+                count_xmit = 0;
+            end
+			else if (count_rec == cycles_rec)
                 begin
-                baud_ticks = ~baudticks;
-                count = count + 1;
+                baud_xmit = 0;
+                count_xmit = 0;
                 end
-            else
-                count = count +1;
+            else 
+                count_xmit = count_xmit + 1;
         end
                 
 endmodule
