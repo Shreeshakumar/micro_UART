0b9d1d325d0c0b8a26bfb2d858203892c53b0655
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-14 19:31:19 +0530

    Refactor uart module parameters and inputs

--- src/design/uart.v
+++ src/design/uart.v
@@ -1,47 +1,42 @@
 `default_nettype none
 
-`include "inc.h"
+module uart #(
+	parameter baudrate	 	= 2400,
+	parameter data_len 	    = 8,
+	parameter clock_rate 	= 100000000,  //100 Mhz	
+	parameter oversample 	= 16
+)(
 
-module baud (
-
-	input 	sys_clk,			//main sys clock
+	input wire 	sys_clk,			//main sys clock
 			sys_rst_l,			//main sys reset
 			xmitH,				//active high -> uart clock with baudrate pulse starts the trasmit process
 			uart_REC_dataH,		//asynchromous input data
-	input	[data_len - 1:0]xmit_dataH,	//data to be sent, This data is sample when xmith high
+	input wire	[data_len - 1:0]xmit_dataH,	//data to be sent, This data is sample when xmith high
 
-	output	uart_XMIT_dataH,	//ouput asynchronous transmitter
+	output wire	uart_XMIT_dataH,	//ouput asynchronous transmitter
 			xmit_doneH,			//when active high thhis indictes that the xmit_dataH has been fully transmitted
 			rec_readyH,			//when high indicates fresh data is available on rec_dataH 
 			rec_busy,			//indicatedd if the receiver is currently busy
 			xmit_active,		//indicates if the transmitter is actively transmitting a data
-	output	[data_len - 1:0]rec_dataH,		//de-serialized recieved from the remote
+	output wire	[data_len - 1:0]rec_dataH		//de-serialized recieved from the remote
 	
 			);
 
-	/*(in inc.h)
-	DEFAULT PARAMETERS: 
-	parameter baudrate	 	= 2400;
-	parameter data_length 	= 8;
-	parameter clock_rate 	= 1000 000 000;  //100 Mhz
-	parameter oversample 	= 16;
-	*/
-	
-	wire 
+wire baud_rec,baud_xmit;
 
-	u_baud	baud(
-		.sys_rst_l(sys_rst_l), .xmitH(xmitH), .sys_clk(sys_clk),		//Main_inputs
+	baud	baud(
+		.sys_rst_l(sys_rst_l), .sys_clk(sys_clk),		//Main_inputs
 		.baud_rec(baud_rec), .baud_xmit(baud_xmit)						//Wires
 		);
 
 	u_xmit 	xmit(
 		.sys_rst_l(sys_rst_l), .xmitH(xmitH), .xmit_dataH(xmit_dataH),							//Main_inputs
-		.uart_XMIT_dataH(uart_XMIT_dataH), .xmit_doneH(xmit_doneH), .xmit_active(xmit_active)	//Main_outputs
+		.uart_XMIT_dataH(uart_XMIT_dataH), .xmit_doneH(xmit_doneH), .xmit_active(xmit_active),	//Main_outputs
 		.baud_xmit(baud_xmit)																	//Wires
 		);
 
 	u_rec	rec(
-		.sys_rst_l(sys_rst_l), .xmitH(xmitH), .uart_REC_dataH(uart_REC_dataH),		//Main_inputs
+		.sys_rst_l(sys_rst_l), .xmit_active(xmit_active), .uart_XMIT_dataH(uart_REC_dataH),		//Main_inputs
 		.rec_readyH(rec_readyH), .rec_busy(rec_busy), .rec_dataH(rec_dataH)			//Main_outputs
 		.baud_rec(baud_rec)															//Wires
 		);
