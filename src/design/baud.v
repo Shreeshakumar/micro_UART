a3e04fa47b77a326cfde8b63c8dc1830aca6f556
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-14 19:39:41 +0530

    Refactor baud module with improved formatting

--- src/design/baud.v
+++ src/design/baud.v
@@ -1,25 +1,24 @@
 `default_nettype none
 
-module baud #(
+module baud #(		//default values
 	parameter baudrate	 	= 2400,
 	parameter data_len 	    = 8,
-	parameter clock_rate 	= 100000000,  //100 Mhz	
-	parameter oversample 	= 16,
-
-	parameter cycles_rec = (clock_rate/(baudrate*oversample)) ,//cycle is bit time * clock
-	parameter cycles_xmit = (clock_rate/baudrate) //cycle is bit time * clock
+	parameter clock_rate 	= 100_000_000,  //100 Mhz	
+	parameter oversample 	= 16
 )(
-		input wire 	sys_rst_l,         //main sys reset
-        			sys_clk,                 //main sys clock
-		output reg  baud_rec, baud_xmit						 //Wire
+		input wire 	sys_rst_l,         		//main sys reset
+        			sys_clk,                //main sys clock
+		output reg  baud_rec, baud_xmit		//Wire
 
 		);
-	
+	localparam  cycles_rec = (clock_rate/(baudrate*oversample)) ,//cycle is bit time * clock
+	localparam cycles_xmit = (clock_rate/baudrate) //cycle is bit time * clock
 
 	//reg [$clog2(cycles_rec)-1:0] count_rec;
 	reg [$clog2(cycles_xmit)-1:0] count_xmit;
 	reg [$clog2(cycles_rec)-1:0] count_rec;
-    initial $display(cycles_xmit,"cycles of rx");
+    //initial $display(cycles_xmit,"cycles of rx");
+	//initial $display(cycles_rec,"cycles of rx");
 
     always@(posedge sys_clk or posedge sys_rst_l)
         begin
@@ -47,7 +46,7 @@ module baud #(
                 baud_xmit <= 0;
                 count_xmit <= 0;
             end
-			else if (count_xmit == cycles_xmit-1)
+			else if (count_xmit == cycles_xmit - 1)
                 begin
                 baud_xmit <= 1;
                 count_xmit <= 0;
@@ -58,5 +57,5 @@ module baud #(
                 count_xmit <= count_xmit + 1;
 				end
         end
-                
+
 endmodule
