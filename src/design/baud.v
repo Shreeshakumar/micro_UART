bb3513811cf315321d9f13c5eea48889bc1b8e67
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-14 19:28:46 +0530

    Update baud.v

--- src/design/baud.v
+++ src/design/baud.v
@@ -4,19 +4,22 @@ module baud #(
 	parameter baudrate	 	= 2400,
 	parameter data_len 	    = 8,
 	parameter clock_rate 	= 100000000,  //100 Mhz	
-	parameter oversample 	= 16
-	)(
-		input wire 	sys_rst_l,         		 //main sys reset
+	parameter oversample 	= 16,
+
+	parameter cycles_rec = (clock_rate/(baudrate*oversample)) ,//cycle is bit time * clock
+	parameter cycles_xmit = (clock_rate/baudrate) //cycle is bit time * clock
+)(
+		input wire 	sys_rst_l,         //main sys reset
         			sys_clk,                 //main sys clock
-		output reg  baud_rec, baud_xmit		 //Wire
+		output reg  baud_rec, baud_xmit						 //Wire
+
 		);
 	
-	localparam cycles_rec = (clock_rate/(baudrate*oversample)) ;//cycle is bit time * clock
-	localparam cycles_xmit = (clock_rate/baudrate) ;//cycle is bit time * clock
-	
+
 	//reg [$clog2(cycles_rec)-1:0] count_rec;
 	reg [$clog2(cycles_xmit)-1:0] count_xmit;
 	reg [$clog2(cycles_rec)-1:0] count_rec;
+    initial $display(cycles_xmit,"cycles of rx");
 
     always@(posedge sys_clk or posedge sys_rst_l)
         begin
