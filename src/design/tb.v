da061888b91b1cf4cc34f3829b69632ae079c75f
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-14 19:44:46 +0530

    Refactor testbench parameters and signal assignments

--- src/design/tb.v
+++ src/design/tb.v
@@ -1,24 +1,26 @@
 `timescale 1ns / 1ps
 
-module tb #
-(parameter data_len = 8
+module tb#(		//default values
+	parameter baudrate	 	= 2400,
+	parameter data_len 	    = 8,
+	parameter clock_rate 	= 100_000_000,  //100 Mhz	
+	parameter oversample 	= 16
 );
-reg sys_clk, sys_rst_l;
-reg xmitH;
-reg [data_len-1:0] xmit_dataH;
+	reg sys_clk, sys_rst_l;
+	reg xmitH;
+	reg [data_len-1:0] xmit_dataH;
+	
+	wire uart_XMIT_dataH;
+	wire xmit_doneH;
+	wire xmit_active;
 
-wire uart_XMIT_dataH;
-wire xmit_doneH;
-wire xmit_active;
+	wire baud_rec;
+	wire baud_xmit;
 
-wire baud_rec;
-wire baud_xmit;
-
-wire uart_REC_dataH;
-wire rec_readyH;
-wire rec_busy;
-wire [data_len-1:0]rec_dataH;
-
+	wire uart_REC_dataH;
+	wire rec_readyH;
+	wire rec_busy;
+	wire [data_len-1:0]rec_dataH;
 
 baud dut(.sys_rst_l(sys_rst_l), .sys_clk(sys_clk), .baud_rec(baud_rec), .baud_xmit(baud_xmit));
 
@@ -34,30 +36,26 @@ u_rec	rec(
 		.baud_rec(baud_rec)															//Wires
 		);
 
-assign uart_REC_dataH = uart_XMIT_dataH; 
+assign uart_REC_dataH = uart_XMIT_dataH; //equal equal
 
 initial sys_clk = 0;
-always #5 sys_clk = ~sys_clk;
+always #10 sys_clk = ~sys_clk;
 
 initial begin
-@(posedge sys_clk);
-sys_rst_l = 1;
-
 @(posedge sys_clk);
 sys_rst_l = 0;
 
 @(posedge sys_clk);
-xmitH = 1;
+sys_rst_l = 1;
+
+@(posedge sys_clk);
+xmitH = 0;
 
 @(posedge sys_clk);
 xmit_dataH = 8'h0f;
 
-#1000000;
-xmitH = 0;
-
-
-
+@(posedge sys_clk);
+xmitH = 1;
 
 end
-
 endmodule
