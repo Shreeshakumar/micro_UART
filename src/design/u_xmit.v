64881cca8438f35e26fc452f4ccd0297d449f9b2
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-14 19:30:58 +0530

    Refactor u_xmit module for improved functionality

--- src/design/u_xmit.v
+++ src/design/u_xmit.v
@@ -1,89 +1,83 @@
 `default_nettype none
 
-`include "inc.h"
+module u_xmit #(
+    parameter data_len = 8
+)(
+    input wire sys_rst_l,
+    input wire xmitH,
+    input wire baud_xmit,
 
-module u_xmit (
-    input wire      sys_rst_l,                        //main sys reset 
-                    xmitH,                          //active high -> uart clock with baudrate pulse starts the trasmit process
-                    baud_xmit                       //baud rate for xmith
-    input wire      [data_len-1 : 0]xmit_dataH,		//data to be sent, This data is sample when xmith high    			
-    output wire     uart_XMIT_dataH,                //ouput asynchronous transmitter 
-                    xmit_doneH,                     //when active high thhis indictes that the xmit_dataH has been fully transmitted
-                    xmit_active	                    //indicates if the transmitter is actively transmitting a data
-		);
+    input wire [data_len-1:0] xmit_dataH,
 
-	//state parameters
-	parameter IDLE	= 0;
-	parameter START	= 1;
-	parameter SEND	= 2;
-	parameter STOP	= 3;
+    output reg uart_XMIT_dataH,
 
-	//state reg
-	reg [1:0]CS;
-	reg [1:0]NS;
+    output wire xmit_doneH,
+    output wire xmit_active
+);
 
-	reg [data_len-1 : 0]data_ts;
-	reg [$clog2(data_len)-1:0] count_ts;
-	
-	always@(posedge clk or posedge sys_rst_l)
-		begin
-			if(sys_rst_l)
-				CS <= IDLE;
-			else
-				CS <= NS;
-		end
+localparam IDLE  = 2'd0;
+localparam START = 2'd1;
+localparam SEND  = 2'd2;
+localparam STOP  = 2'd3;
 
-	always@(posedge clk)
-		begin
-			case(CS)
-				IDLE	:	begin
-								if(xmit)
-									begin
-										NS = START;
-										data_ts = xmit_dataH;
-										count_ts = 'd0;
-									end
-								else
-									NS = IDLE;
-							end
-				START	:	begin
-								if(baud_xmit)
-									begin	
-										uart_XMIT_dataH = 0;
-										NS = START;
-									end
-								else
-									NS = START;
-							end
-				SEND	:	begin
-								if(baud_xmit)
-									begin	
-										if(count_ts == data_len)
-												NS = STOP;
-										else
-											begin
-												uart_XMIT_dataH = data_ts[count_ts];
-												count_ts = count_ts + 1;
-												NS = START;
-											end
-									end
-								else
-									NS = SEND;
-							end
-				STOP	:	begin
-								if(baud_xmit)
-									begin	
-										uart_XMIT_dataH = 1;
-										NS = IDLE;
-									end
-								else
-									NS = IDLE;
-							end
-				default	:	NS = IDLE;
-			endcase
-		end	
-	
-	assign xmit_doneH = (CS == IDLE)? 1 : 0;                     //when active high thhis indictes that the xmit_dataH has been fully transmitted
-	assign xmit_active = (CS == IDLE)? 0 : 1;  	                    //indicates if the transmitter is actively transmitting a data
-    
+reg [1:0] state;
+
+reg [data_len-1:0] data_ts;
+reg [$clog2(data_len)-1:0] count_ts;
+
+always @(posedge baud_xmit or posedge sys_rst_l)
+begin
+    if(sys_rst_l)
+    begin
+        state <= IDLE;
+        uart_XMIT_dataH <= 1'b1;
+        count_ts <= 0;
+        data_ts <= 0;
+    end
+
+    else
+    begin
+        case(state)
+
+            IDLE:
+            begin
+                uart_XMIT_dataH <= 1'b1;
+
+                if(~xmitH)
+                begin
+                    data_ts <= xmit_dataH;
+                    count_ts <= 0;
+                    state <= START;
+                end
+            end
+
+            START:
+            begin
+                uart_XMIT_dataH <= 1'b0;
+                state <= SEND;
+            end
+
+            SEND:
+            begin
+                uart_XMIT_dataH <= data_ts[count_ts];
+
+                if(count_ts == data_len-1)
+                    state <= STOP;
+                else
+                    count_ts <= count_ts + 1;
+            end
+
+            STOP:
+            begin
+                uart_XMIT_dataH <= 1'b1;
+                state <= IDLE;
+            end
+
+        endcase
+    end
+end
+
+assign xmit_doneH = (state == IDLE);
+assign xmit_active = (state != IDLE);
+
 endmodule
