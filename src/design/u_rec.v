20903493351605f1b5e44b3ced77fd0138f8dc92
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-14 19:52:22 +0530

    Update u_rec.v

--- src/design/u_rec.v
+++ src/design/u_rec.v
@@ -1,8 +1,10 @@
 `default_nettype none
 
-module u_rec #(
-    parameter DATA_LEN = 8,
-    parameter sampling = 16
+module u_rec #(		//default values
+	parameter baudrate	 	= 2400,
+	parameter data_len 	    = 8,
+	parameter clock_rate 	= 100_000_000,  //100 Mhz	
+	parameter oversample 	= 16
 )(
     input  wire                     sys_clk,
     input  wire                     sys_rst_l,
@@ -12,7 +14,7 @@ module u_rec #(
 
     output wire                     rec_readyH,
     output wire                     rec_busy,
-    output reg  [DATA_LEN-1:0]      rec_dataH      
+    output reg  [data_len-1:0]      rec_dataH      
 );
 
     localparam IDLE  = 2'b00;
@@ -21,10 +23,9 @@ module u_rec #(
     localparam STOP  = 2'b11;
 
     reg [1:0] CS, NS;
-
-    reg [$clog2(sampling)-1:0]   sample_cnt;
-    reg [$clog2(DATA_LEN)-1:0]   bit_cnt;
-    reg [DATA_LEN-1:0]           temp;
+    reg [$clog2(oversample)-1:0]   sample_cnt;
+    reg [$clog2(data_len)-1:0]   bit_cnt;
+    reg [data_len-1:0]           temp;
 
     always @(posedge sys_clk or posedge sys_rst_l)
     begin
@@ -36,57 +37,59 @@ module u_rec #(
 
     always @(posedge sys_clk or posedge sys_rst_l)
     begin
-        if (sys_rst_l) begin
+        if (sys_rst_l) 
+            begin
             sample_cnt <= 'd0;
             bit_cnt    <= 'd0;
             temp       <= 'd0;
             rec_dataH  <= 'd0;
-        end
+            end
         else begin
             case (CS)
 
                 IDLE: begin
-                    if (xmit_active && uart_REC_dataH == 1'b0) begin
-                        sample_cnt <= 'd0;
-                    end
-                end
+                        if (xmit_active && uart_REC_dataH == 1'b0) 
+                            sample_cnt <= 'd0;
+                        end
 
                 START: begin
-                    if (baud_rec) begin
-                        if (sample_cnt == sampling - 1) begin
-                            sample_cnt <= 'd0;
-                            bit_cnt    <= 'd0;
-                        end else begin
-                            sample_cnt <= sample_cnt + 'd1;
+                        if (baud_rec) 
+                            begin
+                            if (sample_cnt == oversample - 1) 
+                                begin
+                                sample_cnt <= 'd0;
+                                bit_cnt    <= 'd0;
+                                end 
+                            else 
+                                sample_cnt <= sample_cnt + 'd1;
+                            end
                         end
-                    end
-                end
 
                 REC: begin
-                    if (baud_rec) begin
-                        if (sample_cnt == sampling - 1) begin
-                            temp[bit_cnt] <= uart_REC_dataH;
-                            bit_cnt       <= bit_cnt + 1'b1;
-                            sample_cnt    <= 'd0;
-                            if (bit_cnt == DATA_LEN - 1) begin
-                                rec_dataH <= {uart_REC_dataH, temp[DATA_LEN-2:0]};
+                        if (baud_rec) 
+                            begin
+                            if (sample_cnt == oversample - 1) 
+                                begin
+                                temp[bit_cnt] <= uart_REC_dataH;
+                                bit_cnt       <= bit_cnt + 1'b1;
+                                sample_cnt    <= 'd0;
+                                    if (bit_cnt == DATA_LEN - 1) 
+                                        rec_dataH <= {uart_REC_dataH, temp[data_len-2:0]};
+                                end 
+                            else 
+                                sample_cnt <= sample_cnt + 'd1;
                             end
-                        end else begin
-                            sample_cnt <= sample_cnt + 'd1;
-                        end
                     end
-                end
 
                 STOP: begin
-                    if (baud_rec) begin
-                        if (sample_cnt == sampling - 1) begin
-                            sample_cnt <= 'd0;
-                        end else begin
-                            sample_cnt <= sample_cnt + 'd1;
+                        if (baud_rec) 
+                            begin
+                            if (sample_cnt == sampling - 1) 
+                                sample_cnt <= 'd0;
+                            else 
+                                sample_cnt <= sample_cnt + 'd1;
+                            end
                         end
-                    end
-                end
-
             endcase
         end
     end
@@ -95,30 +98,19 @@ module u_rec #(
     begin
         NS = CS; 
         case (CS)
+            IDLE:    if (xmit_active && uart_REC_dataH == 1'b0)
+                            NS = START;
 
-            IDLE: begin
-                if (xmit_active && uart_REC_dataH == 1'b0)
-                    NS = START;
-            end
+            START:   if (baud_rec && sample_cnt == sampling - 1)
+                            NS = REC;
 
-            START: begin
-                if (baud_rec && sample_cnt == sampling - 1)
-                    NS = REC;
-            end
+            REC:     if (baud_rec && sample_cnt == sampling - 1 && bit_cnt == DATA_LEN - 1)
+                            NS = STOP;
 
-            REC: begin
-                if (baud_rec && sample_cnt == sampling - 1 && bit_cnt == DATA_LEN - 1)
-                    NS = STOP;
-            end
+            STOP:    if (baud_rec && sample_cnt == sampling - 1 && uart_REC_dataH == 1'b1)
+                            NS = IDLE;
 
-            STOP: begin
-                if (baud_rec && sample_cnt == sampling - 1 && uart_REC_dataH == 1'b1)
-                    NS = IDLE;
-            end
-
-            default:
-                NS = IDLE;
-
+            default:    NS = IDLE;
         endcase
     end
 
