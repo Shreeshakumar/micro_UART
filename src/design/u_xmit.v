29db022c76897ea0d78847da2c8606ea9899671a
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-14 19:42:18 +0530

    Update u_xmit.v

--- src/design/u_xmit.v
+++ src/design/u_xmit.v
@@ -1,16 +1,17 @@
 `default_nettype none
 
-module u_xmit #(
-    parameter data_len = 8
+module u_xmit #(		//default values
+	parameter baudrate	 	= 2400,
+	parameter data_len 	    = 8,
+	parameter clock_rate 	= 100_000_000,  //100 Mhz	
+	parameter oversample 	= 16
 )(
     input wire sys_rst_l,
     input wire xmitH,
     input wire baud_xmit,
-
     input wire [data_len-1:0] xmit_dataH,
 
     output reg uart_XMIT_dataH,
-
     output wire xmit_doneH,
     output wire xmit_active
 );
@@ -21,7 +22,6 @@ localparam SEND  = 2'd2;
 localparam STOP  = 2'd3;
 
 reg [1:0] state;
-
 reg [data_len-1:0] data_ts;
 reg [$clog2(data_len)-1:0] count_ts;
 
@@ -38,41 +38,35 @@ begin
     else
     begin
         case(state)
+            IDLE:    begin
+                     uart_XMIT_dataH <= 1'b1;
+                     if(~xmitH)
+                        begin
+                            data_ts <= xmit_dataH;
+                            count_ts <= 0;
+                            state <= START;
+                        end
+                    end
 
-            IDLE:
-            begin
-                uart_XMIT_dataH <= 1'b1;
+            START:    begin
+                        uart_XMIT_dataH <= 1'b0;
+                        state <= SEND;
+                    end
 
-                if(~xmitH)
-                begin
-                    data_ts <= xmit_dataH;
-                    count_ts <= 0;
-                    state <= START;
-                end
-            end
+            SEND:    begin
+                        uart_XMIT_dataH <= data_ts[count_ts];
+                    if(count_ts == data_len-1)
+                        state <= STOP;
+                    else
+                        count_ts <= count_ts + 1;
+                    end
 
-            START:
-            begin
-                uart_XMIT_dataH <= 1'b0;
-                state <= SEND;
-            end
+            STOP:    begin
+                        uart_XMIT_dataH <= 1'b1;
+                        state <= IDLE;
+                    end
 
-            SEND:
-            begin
-                uart_XMIT_dataH <= data_ts[count_ts];
-
-                if(count_ts == data_len-1)
-                    state <= STOP;
-                else
-                    count_ts <= count_ts + 1;
-            end
-
-            STOP:
-            begin
-                uart_XMIT_dataH <= 1'b1;
-                state <= IDLE;
-            end
-
+            default:    state <= IDLE;
         endcase
     end
 end
