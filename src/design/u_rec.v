95151f2d9fcf1fdbd9b5ac98e1dfbc31c3453600
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-14 19:30:08 +0530

    Refactor u_rec module with parameterized data length

--- src/design/u_rec.v
+++ src/design/u_rec.v
@@ -1,88 +1,128 @@
 `default_nettype none
 
-`include "inc.h"
+module u_rec #(
+    parameter DATA_LEN = 8,
+    parameter sampling = 16
+)(
+    input  wire                     sys_clk,
+    input  wire                     sys_rst_l,
+    input  wire                     xmit_active,
+    input  wire                     baud_rec,
+    input  wire                     uart_REC_dataH,
 
-module u_rec (
-    input wire      sys_rst_l,                      //main sys reset 
-                    xmitH,                          //active high -> uart clock with baudrate pulse starts the trasmit process
-                    baud_rec,                       //baud rate for xmith
-                    uart_REC_dataH,                 //asynchromous input data
-    output wire     rec_readyH,                     //when high indicates fresh data is available on rec_dataH
-                    rec_busy,                       //indicatedd if the receiver is currently busy
-    output reg      [data_len - 1:0]rec_dataH       //indicates if the transmitter is actively transmitting a data
-		);
+    output wire                     rec_readyH,
+    output wire                     rec_busy,
+    output reg  [DATA_LEN-1:0]      rec_dataH      
+);
 
-	//state parameters
-	parameter IDLE	= 0;
-	parameter START	= 1;
-	parameter REC	  = 2;
-	parameter STOP	= 3;
+    localparam IDLE  = 2'b00;
+    localparam START = 2'b01;
+    localparam REC   = 2'b10;
+    localparam STOP  = 2'b11;
 
-	//state reg
-	reg [1:0]CS;
-	reg [1:0]NS;
+    reg [1:0] CS, NS;
 
-	reg [$clog2(data_len)-1:0] count_ts;
-	
-	/*always@(posedge clk or posedge sys_rst_l)
-		begin
-			if(sys_rst_l)
-				CS <= IDLE;
-			else
-				CS <= NS;
-		end*/
+    reg [$clog2(sampling)-1:0]   sample_cnt;
+    reg [$clog2(DATA_LEN)-1:0]   bit_cnt;
+    reg [DATA_LEN-1:0]           temp;
 
-  always@(posedge clk)
+    always @(posedge sys_clk or posedge sys_rst_l)
     begin
-      if (sys_rst_l) begin
-        rdy = 0;
-        data_out = 0;
+        if (sys_rst_l)
+            CS <= IDLE;
+        else
+            CS <= NS;
     end
-    end
-  
-    always @(posedge clk)
-        begin
-            if(!xmitH)
-                rdy <= 0;
-            if(baud_rec)
-                begin
-                    case(state)
-                        IDLE :      begin
-                                               if(rx == 0 && sample != 0)
-                                                    sample <= sample + 1'b1;
-                                                if(sample == 15)
-                                                    begin
-                                                        state <= data_out_state;
-                                                        sample <= 0;
-                                                        index <= 0;
-                                                        temp_register <= 0;
-                                                    end
-                                            end
-                        data_out_state :    begin
-                                                sample <= sample + 1'b1;
-                                                if(sample == 4'h8)
-                                                    begin
-                                                        temp_register[index] <= rx;
-                                                        index <= index + 1'b1;
-                                                    end
-                                                if(index == 8 && sample == 15)
-                                                    state <= stop_state;
-                                            end
-                        stop_state :       begin
-                                                if(sample == 15)
-                                                    begin
-                                                        NS = IDLE;
-                                                        data_out <= temp_register;
-                                                        rdy <= 1'b1;
-                                                        sample <= 0;
-                                                    end
-                                                else
-                                                    sample <= sample + 1'b1;
-                                            end
-                        default :           NS = IDLE;
-                end
+
+    always @(posedge sys_clk or posedge sys_rst_l)
+    begin
+        if (sys_rst_l) begin
+            sample_cnt <= 'd0;
+            bit_cnt    <= 'd0;
+            temp       <= 'd0;
+            rec_dataH  <= 'd0;
         end
-	
-    assign rec_readyH = (CS == IDLE)? 1 : 0;                     //when high indicates fresh data is available on rec_dataH
-    assign rec_busy   = (CS == IDLE)? 0 : 1;                       //indicatedd if the receiver is currently busy
+        else begin
+            case (CS)
+
+                IDLE: begin
+                    if (xmit_active && uart_REC_dataH == 1'b0) begin
+                        sample_cnt <= 'd0;
+                    end
+                end
+
+                START: begin
+                    if (baud_rec) begin
+                        if (sample_cnt == sampling - 1) begin
+                            sample_cnt <= 'd0;
+                            bit_cnt    <= 'd0;
+                        end else begin
+                            sample_cnt <= sample_cnt + 'd1;
+                        end
+                    end
+                end
+
+                REC: begin
+                    if (baud_rec) begin
+                        if (sample_cnt == sampling - 1) begin
+                            temp[bit_cnt] <= uart_REC_dataH;
+                            bit_cnt       <= bit_cnt + 1'b1;
+                            sample_cnt    <= 'd0;
+                            if (bit_cnt == DATA_LEN - 1) begin
+                                rec_dataH <= {uart_REC_dataH, temp[DATA_LEN-2:0]};
+                            end
+                        end else begin
+                            sample_cnt <= sample_cnt + 'd1;
+                        end
+                    end
+                end
+
+                STOP: begin
+                    if (baud_rec) begin
+                        if (sample_cnt == sampling - 1) begin
+                            sample_cnt <= 'd0;
+                        end else begin
+                            sample_cnt <= sample_cnt + 'd1;
+                        end
+                    end
+                end
+
+            endcase
+        end
+    end
+
+    always @(*)
+    begin
+        NS = CS; 
+        case (CS)
+
+            IDLE: begin
+                if (xmit_active && uart_REC_dataH == 1'b0)
+                    NS = START;
+            end
+
+            START: begin
+                if (baud_rec && sample_cnt == sampling - 1)
+                    NS = REC;
+            end
+
+            REC: begin
+                if (baud_rec && sample_cnt == sampling - 1 && bit_cnt == DATA_LEN - 1)
+                    NS = STOP;
+            end
+
+            STOP: begin
+                if (baud_rec && sample_cnt == sampling - 1 && uart_REC_dataH == 1'b1)
+                    NS = IDLE;
+            end
+
+            default:
+                NS = IDLE;
+
+        endcase
+    end
+
+    assign rec_readyH = (CS == IDLE);
+    assign rec_busy   = (CS != IDLE);
+
 endmodule
