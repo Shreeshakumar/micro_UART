3e33779f1b7b46f67072886917f6a0a12c518d04
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-18 16:10:24 +0530

    same baud

--- src/design/baud.v
+++ src/design/baud.v
@@ -4,58 +4,34 @@ module baud #(		//default values
 	parameter baudrate	 	= 2400,
 	parameter data_len 	    = 8,
 	parameter clock_rate 	= 100_000_000,  //100 Mhz	
-	parameter oversample 	= 16
+	parameter sampling 	= 16
 )(
 		input wire 	sys_rst_l,         		//main sys reset
         			sys_clk,                //main sys clock
-		output reg  baud_rec, baud_xmit		//Wire
+		output reg  baud_tick		//Wire
 
 		);
-	localparam  cycles_rec = (clock_rate/(baudrate*oversample)) ,//cycle is bit time * clock
-	localparam cycles_xmit = (clock_rate/baudrate) //cycle is bit time * clock
+	localparam  cycles = (clock_rate/(baudrate*sampling)) ;//cycle is bit time * clock
 
-	//reg [$clog2(cycles_rec)-1:0] count_rec;
-	reg [$clog2(cycles_xmit)-1:0] count_xmit;
-	reg [$clog2(cycles_rec)-1:0] count_rec;
-    //initial $display(cycles_xmit,"cycles of rx");
-	//initial $display(cycles_rec,"cycles of rx");
+	reg [$clog2(cycles)-1:0] count;
 
 	always@(posedge sys_clk or negedge sys_rst_l)
         begin
 			if (~sys_rst_l)
             begin
-                baud_rec <= 0;
-                count_rec <= 0;
+                baud_tick <= 0;
+                count <= 0;
             end
-			else if (count_rec == cycles_rec-1)
+			else if (count == cycles - 1)
                 begin
-                baud_rec <= 1;
-                count_rec <= 0;
+                baud_tick <= 1;
+                count <= 0;
                 end
             else 
 				begin
-                baud_rec <= 0;
-                count_rec <= count_rec + 1;
+                baud_tick <= 0;
+                count <= count + 1;
                 end
-        end
-
-	always@(posedge sys_clk or negedge sys_rst_l)
-        begin
-			if (~sys_rst_l)
-            begin
-                baud_xmit <= 0;
-                count_xmit <= 0;
-            end
-			else if (count_xmit == cycles_xmit - 1)
-                begin
-                baud_xmit <= 1;
-                count_xmit <= 0;
-                end
-            else 
-				begin
-                baud_xmit <= 0;
-                count_xmit <= count_xmit + 1;
-				end
         end
 
 endmodule
