1fc6b7a963d12f89e5d3d5fc582547750a94a958
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-19 12:56:46 +0530

    Replace parameters with defines in inc.h

--- src/design/inc.h
+++ src/design/inc.h
@@ -1,4 +1,5 @@
-parameter baudrate	 	= 2400;
-parameter data_length = 8;
-parameter clock_rate 	= 1000 000 000;  //100 Mhz
-parameter oversample 	= 16;
+
+`define baudrate 2400
+`define data_len 8
+`define clock_rate 100_000_000  //100 Mhz	
+`define sampling 16
