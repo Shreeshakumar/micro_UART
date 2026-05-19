72bebc977e950eacc57c11134e2d648a29c70f66
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-19 12:56:26 +0530

    Refactor baud module to use parameters

--- src/design/baud.v
+++ src/design/baud.v
@@ -1,17 +1,14 @@
 `default_nettype none
 
-module baud #(		//default values
-	parameter baudrate	 	= 2400,
-	parameter data_len 	    = 8,
-	parameter clock_rate 	= 100_000_000,  //100 Mhz	
-	parameter sampling 	= 16
-)(
+`include "inc.h"
+
+module baud (
 		input wire 	sys_rst_l,         		//main sys reset
         			sys_clk,                //main sys clock
 		output reg  baud_tick		//Wire
 
 		);
-	localparam  cycles = (clock_rate/(baudrate*sampling)) ;//cycle is bit time * clock
+	localparam  cycles = (`clock_rate/(`baudrate*`sampling)) ;//cycle is bit time * clock
 
 	reg [$clog2(cycles)-1:0] count;
 
