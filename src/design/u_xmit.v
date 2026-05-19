689ccc67449f441c2827897608e9ea113240364e
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-19 12:57:46 +0530

    Refactor u_xmit module to use parameters

--- src/design/u_xmit.v
+++ src/design/u_xmit.v
@@ -1,15 +1,12 @@
 `default_nettype none
 
-module u_xmit #(		//default values
-	parameter baudrate	 	= 2400,
-	parameter data_len 	    = 8,
-	parameter clock_rate 	= 100_000_000,  //100 Mhz	
-	parameter sampling 	= 16
-)(
+`include "inc.h"
+
+module u_xmit (
     input wire sys_rst_l,
     input wire xmitH,
     input wire baud_tick,
-    input wire [data_len-1:0] xmit_dataH,
+    input wire [`data_len-1:0] xmit_dataH,
 
     output reg uart_XMIT_dataH,
     output wire xmit_doneH,
@@ -22,30 +19,11 @@ localparam SEND  = 2'd2;
 localparam STOP  = 2'd3;
 
 reg [1:0] state;
-reg [data_len-1:0] data_ts;
-reg [$clog2(data_len)-1:0] count_ts;
+reg [`data_len-1:0] data_ts;
+reg [$clog2(`data_len)-1:0] count_ts;
 
 reg [3:0]count;
-reg [3:0]temp;
-reg ttttt = 0;
 
-	always @(posedge baud_tick or negedge sys_rst_l)
-begin
-	if(!sys_rst_l)
-    begin
-       if(temp == 4'b1111)
-       begin
-       ttttt <= !ttttt;
-       end
-       else
-       begin
-       temp = temp + 1'b1;
-       end
-    end
-end
-
-    
-    
 	always @(posedge baud_tick or negedge sys_rst_l)
 begin
 	if(~sys_rst_l)
@@ -63,7 +41,7 @@ begin
             IDLE:    begin
                     if(count == 4'b1111)
                     begin
-                        count = 4'd0;
+                        count <= 4'd0;////////////////////////////////////////////////////////////////////////////////////////////
                         uart_XMIT_dataH <= 1'b1;
                      if(xmitH)
                         begin
@@ -73,43 +51,46 @@ begin
                         end
                     end
                     else
-                        begin state <= IDLE;    count = count + 1'b1;  end
+                        begin state <= IDLE;    count <= count + 1'b1;  end////////////////////////////////////////////////////////////////
                     end
 
             START:    begin
                         if(count == 4'b1111)
                         begin
-                        count = 4'd0;
+                        count <= 4'd0;////////////////////////////////////////////////////////////////////////////////////////////
                             uart_XMIT_dataH <= 1'b0;
                             state <= SEND;
                         end
                       else
-                        begin state <= START;    count = count + 1'b1;  end
+                        begin state <= START;    count <= count + 1'b1;  end////////////////////////////////////////////////////////////////
+
                     end
 
             SEND:    begin
             if(count == 4'b1111)
                         begin
-                        count = 4'd0;
+                        count <= 4'd0;////////////////////////////////////////////////////////////////
                         uart_XMIT_dataH <= data_ts[count_ts];
-                    if(count_ts == data_len-1)
+                    if(count_ts == `data_len-1)
                         state <= STOP;
                     else
                         count_ts <= count_ts + 1;
                     end
                     else
-                        begin state <= SEND;    count = count + 1'b1;  end
+                        begin state <= SEND;    count <= count + 1'b1;  end////////////////////////////////////////////////////////////////
+
                     end
 
             STOP:    begin
             if(count == 4'b1111)
                         begin
-                        count = 4'd0;
+                        count <= 4'd0;////////////////////////////////////////////////////////////////
                         uart_XMIT_dataH <= 1'b1;
                         state <= IDLE;
                     end
                     else
-                        begin state <= STOP;    count = count + 1'b1;  end
+                        begin state <= STOP;    count <= count + 1'b1;  end////////////////////////////////////////////////////////////////
+
                     end
 
             default:    state <= IDLE;
