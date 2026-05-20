de6257dbee8118efabb280e1e769b47b2e068acd
Shreesha Kumar M G <125763205+Shreeshakumar@users.noreply.github.com>
2026-05-20 14:32:09 +0530

    Update micro_UART_tb.v

--- src/verification/micro_UART_tb.v
+++ src/verification/micro_UART_tb.v
@@ -2,7 +2,7 @@
 
 `include "inc.h"
 
-module micro_UART_tb
+module micro_UART_tb;
 
     // SAME INPUT signals
     reg sys_clk, sys_rst_l, xmitH;
@@ -10,39 +10,42 @@ module micro_UART_tb
     reg uart_REC_dataH;
 
     // Reference mode signals
-    reg dut_uart_XMIT_dataH;
-    reg [`data_len - 1 : 0]dut_rec_dataH;
-    reg dut_xmit_doneH;
-    reg dut_xmit_active;
-    reg dut_rec_readyH;
-    reg dut_rec_busy;
+    wire dut_uart_XMIT_dataH;
+    wire [`data_len - 1 : 0]dut_rec_dataH;
+    wire dut_xmit_doneH;
+    wire dut_xmit_active;
+    wire dut_rec_readyH;
+    wire dut_rec_busy;
     
     // Reference mode signals
-    reg ref_uart_XMIT_dataH;
-    reg [`data_len - 1 : 0]ref_rec_dataH;
-    reg ref_xmit_doneH;
-    reg ref_xmit_active;
-    reg ref_rec_readyH;
-    reg ref_rec_busy;
+    wire ref_uart_XMIT_dataH;
+    wire [`data_len - 1 : 0]ref_rec_dataH;
+    wire ref_xmit_doneH;
+    wire ref_xmit_active;
+    wire ref_rec_readyH;
+    wire ref_rec_busy;
 
     // Test counters
     integer pass_count = 0;
     integer fail_count = 0;
     integer test_count = 0;
 
+    localparam cycle_apply_rec =`clock_rate / `baudrate * `sampling;
+    localparam delay_apply_rec = (cycle_apply_rec*10)/`sampling;
+    
     // DUT instantiation
     uart    DUT_uart(
         .sys_clk(sys_clk), .sys_rst_l(sys_rst_l), .xmitH(xmitH),
         .xmit_dataH(xmit_dataH),
         .uart_REC_dataH(uart_REC_dataH), 
-        .uart_XMIT_dataH(dut_uart_XMIT_dataH) ,
+        .uart_XMIT_dataH(dut_uart_XMIT_dataH),
         .rec_dataH(dut_rec_dataH),
         .xmit_doneH(dut_xmit_doneH), .xmit_active(dut_xmit_active),
         .rec_readyH(dut_rec_readyH), .rec_busy(dut_rec_busy)
         );
 
     // Reference model instantiation
-    uart    DUT_uart(
+    ref_uart    ref_uart(
         .sys_clk(sys_clk), .sys_rst_l(sys_rst_l), .xmitH(xmitH),
         .xmit_dataH(xmit_dataH),
         .uart_REC_dataH(uart_REC_dataH), 
@@ -54,41 +57,59 @@ module micro_UART_tb
 
  // Clock generation
         initial sys_clk = 0;
-        always #10 sys_clk = ~sys_clk;
+        always #5 sys_clk = ~sys_clk;
  
     // Test DUT with respect to REF
     initial begin
         // Initialize
-        @(posedge CLK);
-        @(posedge CLK);
-        @(posedge CLK);
-        @(posedge CLK);
-        @(posedge CLK);
+        @(posedge sys_clk);
+        @(posedge sys_clk);
+        @(posedge sys_clk);
+        @(posedge sys_clk);
+        @(posedge sys_clk);
 
         // sys_rst_l toggle
-        @(posedge CLK);
+        @(posedge sys_clk);
         sys_rst_l = 0;
-
-        @(posedge CLK);
-        sys_rst_l = 1;
         
-        @(posedge CLK);
-        sys_rst_l = 0;        // Release reset
+        @(posedge sys_clk);
+        sys_rst_l = 1;        // Release reset
 
-        @(posedge CLK);
-        @(posedge CLK);
-        @(posedge CLK);
-        @(posedge CLK);
-        @(posedge CLK);        
+        @(posedge sys_clk);
+        @(posedge sys_clk);
+        @(posedge sys_clk);
+        @(posedge sys_clk);
+        @(posedge sys_clk);        
 
         // Test transmiter Operations
         $display("\n=== Testing transmiter working ===");
-        test_transmitter(8'h0f);
-/*
-        // Test Logical Operations
-        $display("\n=== Testing Logical Operations (MODE=0) ===");
-        test_reciever();
-*/
+        test_transmitter(8'h5a);
+        
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        
+        // sys_rst_l toggle
+        @(posedge sys_clk);
+        sys_rst_l = 0;
+        
+        @(posedge sys_clk);
+        sys_rst_l = 1;        // Release reset
+        
+        
+        // Test reciever Operations
+        $display("\n=== Testing reciever Operations (MODE=0) ===");
+        test_reciever(8'h0f);
+
         // Summary
         $display("\n=== TEST SUMMARY ===");
         $display("Total Tests: %0d", test_count);
@@ -106,36 +127,96 @@ module micro_UART_tb
 
     // Test transmitter operations
     task test_transmitter(
-        input [`data_len -1 : 0]data;
+        input [`data_len -1 : 0]data
     );
         begin
             // apply test
-            apply_test(data, "BASIC_xmit");
+            apply_xmit(data, "BASIC_xmit");
         end
     endtask
-/*
+
     // Test reciever operations
-    task test_reciever();
+    task test_reciever(
+            input [`data_len -1 : 0]data
+            );
         begin
-            apply_test(8'hF0, 8'h0F, 4'b0000, "AND");
+            apply_rec(data, "BASIC_rec");
         end
     endtask
-*/
+
     // Apply test and check
-    task apply_test(
+    task apply_xmit(
         input [`data_len -1 :0]data,
         input [80*8:1] test_name
     );
         begin
-            @(posedge CLK);
+            @(posedge sys_clk);
             xmit_dataH = data;
             xmitH = 1;
-            @(posedge CLK);
+            @(posedge sys_clk);
             xmitH = 1;
+
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+
             
             test_count = test_count + 1;
             
-            if (compare_outputs()) begin
+            if (compare_outputs(1)) begin
+                $display("[PASS] %s: xmit_dataH=0x%h ", 
+                         test_name, data);
+                //display_mismatch();
+                pass_count = pass_count + 1;
+            end else begin
+                $display("[FAIL] %s: xmit_dataH=0x%h ", 
+                         test_name, data);
+                display_mismatch();
+                fail_count = fail_count + 1;
+            end
+        end
+    endtask
+    
+        // Apply test and check
+    task apply_rec(
+        input [`data_len -1 :0]data,
+        input [80*8:1] test_name
+    );
+        begin
+              #delay_apply_rec;
+        uart_REC_dataH = 1;     sys_rst_l = 0;
+              #delay_apply_rec; sys_rst_l = 1;
+              #delay_apply_rec;
+        uart_REC_dataH = 0; 
+              #delay_apply_rec;
+        uart_REC_dataH = data[0];
+              #delay_apply_rec;
+        uart_REC_dataH = data[ 1];
+              #delay_apply_rec;
+        uart_REC_dataH = data[2];
+              #delay_apply_rec;
+        uart_REC_dataH = data[3];
+              #delay_apply_rec;
+        uart_REC_dataH = data[4];
+              #delay_apply_rec;
+        uart_REC_dataH = data[5];
+              #delay_apply_rec;
+        uart_REC_dataH = data[6];
+              #delay_apply_rec;
+        uart_REC_dataH = data[7];
+        #delay_apply_rec;
+        uart_REC_dataH = 1;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+        #delay_apply_rec;
+
+        
+            test_count = test_count + 1;
+            
+            if (compare_outputs(1)) begin
                 $display("[PASS] %s: xmit_dataH=0x%h ", 
                          test_name, data);
                 //display_mismatch();
@@ -150,7 +231,8 @@ module micro_UART_tb
     endtask
 
     // Compare DUT vs REF
-    function compare_outputs();
+    function compare_outputs(
+    input dummy);
         begin
             compare_outputs = 1;
             
@@ -169,19 +251,10 @@ module micro_UART_tb
     // Display mismatch details
     task display_mismatch();
         begin
-                $display("  SYS INPUTS : \n 
-                             sys_rst_l= %b, xmitH= %b, xmit_dataH= 0x%h, uart_REC_dataH= %b \n",
+                $display("  SYS INPUTS : \n  sys_rst_l= %b, xmitH= %b, xmit_dataH= 0x%h, uart_REC_dataH= %b \n\n",
                                  sys_rst_l, xmitH, xmit_dataH, uart_REC_dataH);
     
-                $display("  DUT & REF OUTPUTS :\n 
-                             dut_uart_XMIT_dataH= %b, dut_rec_dataH= 0x%h, \n 
-                             ref_uart_XMIT_dataH= %b, ref_rec_dataH= 0x%h,
-                 
-                             dut_xmit_doneH= %b, dut_xmit_active= %b, \n
-                             ref_xmit_doneH= %b, ref_xmit_active= %b,
-                 
-                             dut_rec_readyH= %b, dut_rec_busy= %b, \n
-                             ref_rec_readyH= %b, ref_rec_busy= %b \n",
+                $display("  DUT & REF OUTPUTS :\n dut_uart_XMIT_dataH= %b, dut_rec_dataH= 0x%h, \n ref_uart_XMIT_dataH= %b, ref_rec_dataH= 0x%h, \n dut_xmit_doneH= %b, dut_xmit_active= %b, \n  ref_xmit_doneH= %b, ref_xmit_active= %b, \n dut_rec_readyH= %b, dut_rec_busy= %b, \n  ref_rec_readyH= %b, ref_rec_busy= %b \n",
              
                                  dut_uart_XMIT_dataH, dut_rec_dataH, ref_uart_XMIT_dataH, ref_rec_dataH,
                                  dut_xmit_doneH, dut_xmit_active, ref_xmit_doneH, ref_xmit_active,
@@ -191,8 +264,8 @@ module micro_UART_tb
 
     // Waveform dump
     initial begin
-        $dumpfile("alu_test.vcd");
-        $dumpvars(0, alu_testbench);
+        $dumpfile("micro_UART_tb.vcd");
+        $dumpvars(0, micro_UART_tb);
     end
 
 endmodule
